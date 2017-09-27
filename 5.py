38d525d8a3ba849f29dea2ab21e88a0c09978b60
Angie Pigna <angie.pigna@gmail.com>
2017-09-27 18:16:09 +0200

    Added solution problem 5

---
+++ 5.py
@@ -0,0 +1,25 @@
+# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+import time
+
+# Smallest Number Divisible by Range of
+def SNDR(n):
+    product = 1
+    dividends = []
+    for i in range(2,n+1):
+        for div in dividends:
+            if i % div == 0:
+                i //= div
+        dividends.append(i)
+        product *= i
+    return product
+
+if __name__ == '__main__':
+    start_time = time.time()
+
+    n = 20
+    for _ in range(1000):
+        SNDR(n)
+    print(SNDR(n))
+
+    print("--- {} seconds ---".format(time.time() - start_time))
